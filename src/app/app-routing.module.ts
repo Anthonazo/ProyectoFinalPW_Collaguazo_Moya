029767c0394d305f21e02fb17ab6e5b4ec56110b
Anthony Moya Ochoa <anthonymoya246@gmail.com>
2024-01-11 19:54:48 -0500

    Primer commit, ingreso de paginas de ingreso, registro y agradecimiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IngresoComponent } from './pages/registo/ingreso/ingreso.component';
+import { AgradecimientoComponent } from './pages/registo/agradecimiento/agradecimiento.component';
+import { RegistoComponent } from './pages/registo/registo/registo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IngresoComponent},
+  {path: 'agradecimiento', component: AgradecimientoComponent},
+  {path: 'registro', component: RegistoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistoComponent } from './pages/registo/registo/registo.component';
+import { RecuperarCuentaComponent } from './pages/registo/recuperar-cuenta/recuperar-cuenta.component';
+import { IngresoComponent } from './pages/registo/ingreso/ingreso.component';
+import { AgradecimientoComponent } from './pages/registo/agradecimiento/agradecimiento.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistoComponent,
+    RecuperarCuentaComponent,
+    IngresoComponent,
+    AgradecimientoComponent
   ],
   imports: [
     BrowserModule,
