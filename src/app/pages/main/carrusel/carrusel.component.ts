193a46a483aca8bd6b60ca3c22dbe468b70bb0d1
DanielCollaguazo2003 <109354364+DanielCollaguazo2003@users.noreply.github.com>
2024-02-06 19:22:10 -0500

    Arreglo el nav y el footer juunto con agregacion de la funcionalidad de responsiva a la pagina

--- src/app/pages/main/carrusel/carrusel.component.ts
+++ src/app/pages/main/carrusel/carrusel.component.ts
@@ -12,12 +12,35 @@ export class CarruselComponent {
     '/assets/img/carrusel/segunda.png',
     '/assets/img/carrusel/tercera.png'
   ];
+  touchStartX = 0;
+  touchEndX = 0;
+
+  constructor() {}
+
+  setTouchStartX(event: TouchEvent) {
+    this.touchStartX = event.touches[0].clientX;
+  }
+
+  setTouchEndX(event: TouchEvent) {
+    this.touchEndX = event.changedTouches[0].clientX;
+    this.handleTouchMove();
+  }
+
+  handleTouchMove() {
+    if (this.touchStartX - this.touchEndX > 75) {
+      // Deslizar hacia la izquierda
+      this.next();
+    } else if (this.touchEndX - this.touchStartX > 75) {
+      // Deslizar hacia la derecha
+      this.previous();
+    }
+  }
 
   next() {
     this.currentIndex = (this.currentIndex + 1) % this.images.length;
   }
 
   previous() {
-    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+    this.currentIndex = (this.currentIndex + this.images.length - 1) % this.images.length;
   }
 }
